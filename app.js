6a61d7fa1de1a3c0d785c6511b228596060a6a1e
root <root@ip-172-31-30-5.(none)>
2014-06-11 15:19:09 +0000

    init

---
+++ app.js
@@ -0,0 +1,59 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var request = require('request')
+var app = express()
+var router = express.Router()
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+var keys = require('./keys')
+var restaurants = ['Shake Shack', 'Capriottis', 'Grill Fish', 'Chipotle', 'Qdoba', 'Taylor Gourmet', 'Chopt', 'District Taco', 'Pei Wei', 'McDonalds', 'Potbelly', 'Noodles and Company', 'Julies Empanadas', 'Bub & Pops', 'Sweetgreen', 'Moby Dick', 'Panera', 'Roti', 'Jimmy Johns']
+
+function getRandomInt(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Gif Oracle
+app.post('/gifme', function(req, res) {
+  // Get info from the post
+  var text = req.body.text.split('-c')
+  var gifQuery = text[0]
+  // Get gifs from giphy 
+  request('http://api.giphy.com/v1/gifs/search?q='+gifQuery+keys.gifme, function(err, response, body) {
+    if (err) res.send(err)
+    var b = JSON.parse(body), random = Math.floor(Math.random() * (b.data.length - 1)) + 0
+    var payload;
+    var url = b.data[random].url
+      , user = req.body.user_name
+      , caption = text[1]
+      , gifTitle = text[0];
+    if (caption) payload ="Title: "+ gifTitle +'\nSent By '+ user + "\n~'"+caption+"'\n<"+url+">" ;
+    else if (!caption) payload = "Title: "+ gifTitle +'\nSent By '+ user + "\n<"+url+">" ;
+    var options = {
+      url: 'Your web hook integration url token here',
+      method: "POST",
+      json: {"text":payload, "username":"the bot name", "icon_emoji":":an emoji:"}
+    }
+    // Send gifs to slack channel
+    request(options, function(err) {
+      if (err) res.send(err);
+    })
+    res.send('Gif sent.')
+  })
+})
+
+// Food Oracle
+app.post('/wheretoeat', function(req, res) {
+    res.send(restaurants[getRandomInt(0, restaurants.length)])
+})
+
+app.post('/helpme', function(req, res) {
+  var help = '/gifme keyword [-c "your caption"]\nreturns a gif with the keyword\n\n/wheretoeat \nreturns a place to eat'
+  res.send(help);
+})
+
+app.use(router)
+app.listen(3000, function() {
+  console.log('listining on port 3000')
+})
---
+++ keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  gifme: '&api_key=dc6zaTOxFJmzC'
+}
