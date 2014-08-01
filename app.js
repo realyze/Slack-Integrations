7a83b4a16c519040c01b2be21d7489a0925b608a
Tomas Brambora <tomas.brambora@gmail.com>
2014-08-01 09:44:20 +0200

    Get a random GIF (with the specified tag).

--- app.js
+++ app.js
@@ -11,11 +11,12 @@ app.get('/gifme', function(req, res) {
   // Get info from the post
   var text = req.query.text
   if (!text) {return res.send(500, 'missing tag');}
+
   // Get gifs from giphy 
-  request('http://api.giphy.com/v1/gifs/search?q='+ text + "&api_key=" + process.env.GIPHY_API_KEY, function(err, response, body) {
+  request('http://api.giphy.com/v1/gifs/random?tag=' + text + 
+      "&api_key=" + process.env.GIPHY_API_KEY, function(err, response, body) {
     if (err) { return res.send(500, err); }
     var b = JSON.parse(body);
-    console.log(b);
     var random = Math.floor(Math.random() * (b.data.length - 1)) + 0
     var payload;
     var obj = b.data[random];
@@ -36,7 +37,7 @@ app.get('/gifme', function(req, res) {
       if (err) {
         return res.send(err);
       } else {
-        res.send('Gif sent. POSTed ' + JSON.stringify(options));
+        res.send(200);
       }
     })
   })
